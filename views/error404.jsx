7210dd9892278df1c9d2a3f57c58dee395904742
Khalid <Alsaoork@gmail.com>
2024-04-01 13:32:17 +0300

    Adding Bootstrap grid to index page

--- views/error404.jsx
+++ views/error404.jsx
@@ -8,6 +8,12 @@ function error404 () {
           <main>
               <h1>404: PAGE NOT FOUND</h1>
               <p>Ooops, sorry, we can't find this page!</p>
+              <div>
+                <img src='/images/cute-cat.jpg' alt="cute cat photo"></img>
+              </div>
+              <div>
+              Photo by <a href="https://unsplash.com/@madhatterzone?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Manja Vitolic</a> on <a href="https://unsplash.com/photos/black-and-white-cat-lying-on-brown-bamboo-chair-inside-room-gKXKBY-C-Dk?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>
+              </div> 
           </main>
       </Def>
     )
--- views/home.jsx
+++ views/home.jsx
@@ -7,6 +7,12 @@ function home () {
       <Def>
           <main>
               <h1>HOME</h1>
+              <div>
+                <img src='/images/salad_and_juice.jpg' alt="salad and juice on black background"></img>
+              </div>
+              <div>
+              Photo by <a href="https://unsplash.com/@shootdelicious?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Eiliv Aceron</a> on <a href="https://unsplash.com/photos/cooked-food-on-black-bowl-ZuIDLSz3XLg?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>
+              </div>  
               <a href="/places">
                 <button className="btn-primary">Places Page</button>
               </a>
