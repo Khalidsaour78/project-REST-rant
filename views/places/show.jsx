56aa746f06961bb00d5b622e425062f4c685a84f
Khalid <Alsaoork@gmail.com>
2024-04-15 00:57:44 +0300

    Part 10: Populating the comments

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -2,6 +2,25 @@ const React = require("react");
 const Def = require("../default");
 
 function show(data) {
+  let comments = (
+    <h3 className="inactive">
+      No comments yet!
+    </h3>
+  )
+  if (data.place.comments.length) {
+    comments = data.place.comments.map((c) => {
+      return (
+        <div className='border'>
+          <h2 className='rant'>{c.rant ? "Rant! 😡" : "Rave! 😻"}</h2>
+          <h4>{c.content}</h4>
+          <h3>
+            <stong>- {c.author}</stong>
+          </h3>
+          <h4>Rating: {c.stars}</h4>
+        </div>
+      );
+    });
+  }
   return (<Def>
     <main>
       <div className="row">
@@ -40,10 +59,10 @@ function show(data) {
          
       </div>
       </div>
-      <div classname="row">
-        <h2>Comments</h2>
-        <h4>No comments yet!</h4>
-      </div>
+      
+      <h2>Comments</h2>
+      {comments}
+      
     </main>
   </Def>
 
