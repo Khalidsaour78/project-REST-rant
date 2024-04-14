f741d5d408bff741bcf22dde52d6b54dffbc710b
Khalid <Alsaoork@gmail.com>
2024-04-15 01:12:35 +0300

    Part 10: Creating a comment

--- controllers/places.js
+++ controllers/places.js
@@ -7,7 +7,6 @@ router.get("/", (req, res) => {
       res.render("places/index", { places });
     })
     .catch((err) => {
-      console.log(err);
       res.render("error404");
     });
 });
@@ -18,8 +17,17 @@ router.post("/", (req, res) => {
       res.redirect("/places");
     })
     .catch((err) => {
-      console.log("err", err);
-      res.render("error404");
+      if (err && err.name == "ValidationError") {
+        let message = "Validation Error: ";
+        for (var field in err.errors) {
+          message += `${field} was ${err.errors[field].value}. `;
+          message += `${err.errors[field].message}`;
+        }
+        console.log("Validation error message", message);
+        res.render("places/new", { message });
+      } else {
+        res.render("error404");
+      }
     });
 });
 
@@ -29,7 +37,9 @@ router.get("/new", (req, res) => {
 
 router.get("/:id", (req, res) => {
   db.Place.findById(req.params.id)
+    .populate("comments")
     .then((place) => {
+      console.log(place.comments);
       res.render("places/show", { place });
     })
     .catch((err) => {
@@ -48,6 +58,32 @@ router.delete("/:id", (req, res) => {
 
 router.get("/:id/edit", (req, res) => {
   res.send("GET edit form stub");
+});
+
+router.post("/:id/comment", (req, res) => {
+  if (req.body.rant) {
+    req.body.rant = true;
+  } else {
+    req.body.rant = false;
+  }
+  db.Place.findById(req.params.id)
+    .then((place) => {
+      console.log(db.Comment);
+      db.Comment.create(req.body)
+        .then((comment) => {
+          place.comments.push(comment.id);
+          place.save().then(() => {
+            res.redirect(`/places/${req.params.id}`);
+          });
+        })
+        .catch((err) => {
+          console.log(err);
+          res.render("error404");
+        });
+    })
+    .catch((err) => {
+      res.render("error404");
+    });
 });
 
 router.post("/:id/rant", (req, res) => {
